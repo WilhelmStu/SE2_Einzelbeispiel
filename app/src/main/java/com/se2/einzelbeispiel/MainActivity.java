527c74f488e88d01f8be3bd45f596673397b8687
willistpf <willi.stuhle@gmail.com>
2020-03-09 21:53:56 +0100

    2.2) "Quersumme der MatNr zu Binärzahl" function added

--- app/src/main/java/com/se2/einzelbeispiel/MainActivity.java
+++ app/src/main/java/com/se2/einzelbeispiel/MainActivity.java
@@ -25,6 +25,13 @@ public class MainActivity extends AppCompatActivity {
         new SendTask().execute(matNr);
     }
 
+    public void calcBin(View view){
+
+        String matNr = ((EditText) findViewById(R.id.mat_eingabe)).getText().toString();
+
+        new CalcTask().execute(matNr);
+    }
+
 
     public class SendTask extends AsyncTask<String, String, String> {
 
@@ -40,6 +47,36 @@ public class MainActivity extends AppCompatActivity {
             super.onPostExecute(s);
             TextView textView = findViewById(R.id.server_antwort);
             textView.setText(s);
+
+        }
+    }
+
+    public class CalcTask extends AsyncTask<String, String, String> {
+
+        @Override
+        protected String doInBackground(String... matNr) {
+
+            int quersumme = 0;
+            for (int i = 0; i < matNr[0].length(); i++) {
+                quersumme += Character.getNumericValue(matNr[0].charAt(i));
+            }
+
+            StringBuilder bin = new StringBuilder();
+            if (quersumme <= 0) bin.append(0);
+
+            while(quersumme != 0){
+                bin.append(quersumme % 2);
+                quersumme /= 2;
+            }
+
+            return "Quersumme als Binärzahl: " + bin.reverse().toString();
+        }
+
+        @Override
+        protected void onPostExecute(String bin) {
+            super.onPostExecute(bin);
+            TextView textView = findViewById(R.id.server_antwort);
+            textView.setText(bin);
 
         }
     }
